38b71e4ce19037c9ee8e54b6e6c6a0c3ad8dc213
Rinat <sinnsofvalkyrie@gmail.com>
2019-01-29 18:06:06 +0400

    Updates

---
+++ precache-manifest.59f7d0b122297d9f0f319ebebad27aa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8210532cb763723fdd82",
+    "url": "/react-redux-todo-tutorial/static/css/main.215ee68b.chunk.css"
+  },
+  {
+    "revision": "8210532cb763723fdd82",
+    "url": "/react-redux-todo-tutorial/static/js/main.8210532c.chunk.js"
+  },
+  {
+    "revision": "07256ee7696cac8be72e",
+    "url": "/react-redux-todo-tutorial/static/js/1.07256ee7.chunk.js"
+  },
+  {
+    "revision": "13651564ad32fb1a4a72",
+    "url": "/react-redux-todo-tutorial/static/js/runtime~main.13651564.js"
+  },
+  {
+    "revision": "3212b86789f04b6fb8859d1299a498b3",
+    "url": "/react-redux-todo-tutorial/index.html"
+  }
+];
